d40f8d012487ef0dbd41769927fd8f8ea5e92931
Tanat0s <berserk.bellato@yandex.ru>
2022-01-24 10:13:06 +0300

    first commit
    Added play button, main menu with level selection, pause prefab, animations, random animation for wall.

---
+++ Assets/Scripts/AnimationRandomBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRandomBehaviour : StateMachineBehaviour
+{
+    // OnStateMachineEnter is called when entering a state machine via its Entry Node
+    override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    {
+       animator.SetInteger("AnimationId", Random.Range(0,5));
+    }
+}
